e5da7b2fef9f7ec536b62e79465c2e4b7b24b311
Bonnie Tsao <IceJade1788@gmail.com>
2021-11-14 12:24:44 -0800

    setup username input form

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { SocketContext, socket } from '../services/socket.js';
 
 function ChatBox() {
-  const [ username, setUsername ] = useState('Bob');
-  const [ nameInput, setNameInput ] = useState('');
+  const [ username, setUsername ] = useState('');
+  const [ usernameInput, setUsernameInput ] = useState('');
 
+  const handleFormChange = (e) => {
+    setUsernameInput(e.target.value);
+  };
 
-  //socket makes connection regardless of view - need to make changes so that connection only occurs after username is entered.
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!usernameInput) {
+      return alert('Username cannot be empty!');
+    }
+    setUsername(usernameInput);
+    setUsernameInput('');
+  };
+
+  //socket makes connection regardless of view - need to make changes so that connection only occurs after username is entered. Issue with multiple connection still?
   return username ? (
     <SocketContext.Provider value={socket}>
       <Chatroom username={username} />
     </SocketContext.Provider>
   ) : (
-    <div>
-      <h2>Username Form</h2>
+    <div className='userNameForm'>
+      <h2>Please enter a username</h2>
+      <form onSubmit={(e) => handleFormSubmit(e)}>
+        <input id='username' onChange={(e) => handleFormChange(e)} required />
+        <button type='submit'>Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
 function Chatroom({ username }) {
